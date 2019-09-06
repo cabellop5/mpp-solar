dae979f9ec371a3bc3faf576f9df67b0ad1ac80f
Pablo Cabello <cabellop5@gmail.com>
2019-09-06 12:55:55 +0200

    log

--- mppsolar/mppcommands.py
+++ mppsolar/mppcommands.py
@@ -194,7 +194,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--command', help='Command to run', default='QID')
     args = parser.parse_args()
 
-    logging.basicConfig(level='DEBUG')
+    logging.basicConfig(filename='mppsolar.log', level='DEBUG')
 
     mp = mppCommands("TEST")
     cmd = mp.execute(args.command)
